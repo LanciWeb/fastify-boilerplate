c5f27afe7fddbfc33376ae3002bd555cd0e57ded
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-29 18:19:48 +0200

    added docs

---
+++ docs/00 - initial_setup.js
@@ -0,0 +1,44 @@
+//* INITIAL SETUP
+
+//| npm init
+//| npm install fastify --save
+//| npm install mongoose --save
+//| npm install nodemon --save-dev
+
+// TODO - Setup Nodemon
+/**
+ * ? package.json
+ * | "dev": "nodemon index.js"
+ */
+
+// TODO - Server setup
+/**
+ * ? index.js
+ * we require the library and declare the app.
+ * We can define the routes  and listen to a given port, just like in Express.js
+ */
+
+//TODO - connect MongoDB
+/**
+ * ? index.js
+ */
+
+//TODO - Define a User Schema and create a User model
+/**
+ * ? models/User.js
+ */
+
+// TODO - Define controllers
+/**
+ * ? controllers/userController.js
+ */
+
+//TODO - Define routes
+/**
+ * ? routes/userRoutes.js
+ */
+
+//TODO - Import the routes in the app
+/**
+ * ? index.js
+ */
