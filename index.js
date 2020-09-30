d31f004d8e3505f2f6a2b6ebb82fdb65e1b263d6
DESKTOP-CGNLPA7\lance <lancellotti.marco@yahoo.it>
2020-09-30 14:58:57 +0200

    imported and registered cookie, caching, serverSession and Auth0

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const fastify = require('fastify');
-const app = fastify();
 const mongoose = require('mongoose');
+const auth0 = require('fastify-auth0');
+const fastifyCookie = require('fastify-cookie');
+const fastifyCashing = require('fastify-caching');
+const auth0Config = require('./src/config/auth0');
 const userRoutes = require('./src/routes/userRoutes');
+const serverSession = require('fastify-server-session');
 const mongoUrl = 'mongodb://localhost:27017/fastify_users';
+const serverConfig = require('./src/config/server_session');
+const app = fastify();
 /** connect to MongoDB datastore */
 try {
   mongoose.connect(mongoUrl, {
@@ -12,8 +18,10 @@ try {
 } catch (error) {
   console.error(error);
 }
-
-userRoutes(app);
+app.register(fastifyCookie);
+app.register(fastifyCashing);
+app.register(serverSession, serverConfig);
+app.register(auth0, auth0Config);
 
 app.listen(3000, (err, address) => {
   if (err) {
